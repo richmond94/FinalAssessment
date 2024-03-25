78a45ea095a68564a0aafcb14675c6cf3781403d
richmond94 <Franticrichmond94@gmail.com>
2024-03-25 17:49:08 +0000

    Result Class has been created

---
+++ FinalAssessment/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssessment
+{
+    public class Result
+    {
+        public int placed { get; private set; }
+        public double raceTime { get; private set; }
+        public bool qualified { get; private set; }
+
+        public Result(int placed, double raceTime, bool qualified)
+        {
+            this.placed = placed;
+            this.raceTime = raceTime;
+            this.qualified = qualified;
+        }
+
+        public bool isQualified()
+        {
+            return placed <= 3;
+        }
+
+        public override string ToString()
+        {
+            return $"Placed: {placed}\n" +
+                   $"Race Time: {raceTime}s\n" +
+                   $"Qualified: {(qualified ? "Yes" : "No")}";
+        }
+    }
+}
