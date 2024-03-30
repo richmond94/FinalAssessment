d676ce25207226f1fd2a97802d1cb417c6e67611
richmond94 <Franticrichmond94@gmail.com>
2024-03-30 10:01:19 +0000

    Competitor class created and tested.

---
+++ FinalAssessment/Competitor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssessment
+{
+    public class Competitor
+    {
+        private int compNumber;
+        private string compName;
+        private int compAge;
+        private string hometown;
+        private bool newPB;
+        private BreastStroke compEvent;
+        private Result results;
+        private CompHistory history;
+
+        public Competitor(int compNumber, string compName, int compAge, string hometown)
+        {
+            this.compNumber = compNumber;
+            this.compName = compName;
+            this.compAge = compAge;
+            this.hometown = hometown;
+            this.newPB = false; 
+        }
+
+        public override string ToString() 
+        { 
+            return $"Competitor Number: {compNumber}\n" +
+                   $"Name: {compName}\n" +
+                   $"Age: {compAge}\n" +
+                   $"Hometown: {hometown}";
+        }
+
+        public bool IsNewPB(bool v)
+        {
+            if (results == null || history == null)
+                return false; 
+
+            
+            if (!newPB || results.raceTime < history.personalBest)
+            {
+                
+                history.personalBest = results.raceTime;
+                newPB = true;
+                return true; 
+            }
+
+            return false; 
+        }
+
+        
+
+        public string ToFile()
+        {
+
+            string filePath = @"D:\College Submissions\OOPS\FinalAssessmentCsv\Competitor.csv";
+
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+
+                writer.WriteLine("Competitor Number,Competitor Name,Age,Town,Personal Best?");
+                writer.WriteLine($"{compNumber},{compName},{compAge},{hometown},{(newPB ? "Yes" : "No")}");
+
+            }
+
+            return filePath;
+        }
+
+    }
+}
--- FinalAssessment/Program.cs
+++ FinalAssessment/Program.cs
@@ -4,14 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+
+
+
         BreastStroke breastStroke = new BreastStroke(1, "Pool A", 123, "2024-03-21 10:00:00", 60.5, "Breaststroke", 100, 65.2, true);
+        Competitor competitor = new Competitor(1, "Walter White", 25, "New Mexico");
+        competitor.IsNewPB(true);
 
         Console.WriteLine(breastStroke.ToString());
-
+        Console.WriteLine(competitor.ToString());
         string filePath = breastStroke.ToFile();
+        filePath = competitor.ToFile();
 
-        
         if (File.Exists(filePath))
         {
             Console.WriteLine($"CSV file created at: {filePath}");
@@ -20,5 +24,6 @@ class Program
         {
             Console.WriteLine("Failed to create CSV file.");
         }
+
     }
 }
